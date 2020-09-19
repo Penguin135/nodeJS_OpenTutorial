d1e53e8f5f1ce2f85263e70ae1d9d0f66c4d31e3
Penguin135 <rudwns273@naver.com>
2020-09-19 16:16:42 +0900

    author curd 완성

---
+++ OpenTutorial-mysql/lib/db.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host : '172.30.1.53',
+    user : 'Open',
+    password : 'Piano11823!',
+    database : 'opentutorials'
+});
+db.connect();
+
+module.exports=db;
--- OpenTutorial-mysql/lib/template.js
+++ OpenTutorial-mysql/lib/template.js
@@ -9,12 +9,12 @@ var template = {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
+      <a href="/author">author</a>
       <ol>
         ${list}
       </ol>
       ${control}
       ${body}
-      </p>
     </body>
     </html>
     `;
@@ -32,7 +32,7 @@ var template = {
     authors.forEach(author => {
       if (author_id === author.id) {
         selected = ' selected';
-      }else{
+      } else {
         selected = '';
       }
       tag = tag + `<option value="${author.id}" ${selected}>${author.name}</option>\n`;
@@ -40,6 +40,28 @@ var template = {
     return `<select name="author">
                 ${tag}
               </select>`;
+  },
+  authorTable: function (authors) {
+    var table = `<table border="1px" style="border-collapse: collapse;">
+                <tr>
+                    <td>이름</td>
+                    <td>프로필</td>
+                    <td>수정</td>
+                    <td>삭제</td>`;
+    authors.forEach(function (author) {
+      table += `<tr>
+                    <td>${author.name}</td>
+                    <td>${author.profile}</td>
+                    <td><a href="/author/update?id=${author.id}">update</a></td>
+                    <td>
+                      <form action="/author/delete_process" method="POST">
+                        <input type="hidden" name="id" value="${author.id}">
+                        <input type="submit" value="delete">
+                      </form>
+                    </td></tr>`
+    });
+    table += `</table>`;
+    return table;
   }
 }
 module.exports = template;

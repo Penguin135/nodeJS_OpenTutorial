e3e4dfea420636843cc1304865394415e124cbba
Penguin135 <rudwns273@naver.com>
2020-09-25 21:44:49 +0900

    cookie를 이용한 로그인 및 기능 제어 구현

--- OpenTutorial-filestream/main.js
+++ OpenTutorial-filestream/main.js
@@ -5,23 +5,50 @@ var qs = require('querystring');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template')
+var cookie = require('cookie');
 //정리용 함수
 
 
+function authIsOwner(request, response){
+    var isOwner = false;
+
+    var cookies={};
+    if (request.headers.cookie) {
+        cookies = cookie.parse(request.headers.cookie);
+        
+    }
+
+    if(cookies.email =='rudwns273@naver.com' && cookies.password =='111'){
+        isOwner = true;
+    }
+    
+    return isOwner;
+}
+
+function authStatusUI(isOwner){
+    var UI = `<a href='/login'>login</a>`;
+    if(isOwner){
+        UI = `<a href='/logout_process'>logout</a>`
+    }
+    return UI;
+}
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     
+    var isOwner = authIsOwner(request, response);
+    console.log(isOwner);
 
+    
     if (pathname === '/') {
         if (queryData.id === undefined) {
             fs.readdir('./data', function (err, filelist) {
                 var title = 'Welcome';
                 var list = template.list(filelist);
                 var description = 'Hello, Node.js';
-                var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+                var html = template.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`, authStatusUI(isOwner));
                 response.writeHead(200);
                 response.end(html);
             });
@@ -38,13 +65,17 @@ var app = http.createServer(function (request, response) {
                     <form action="/delete_process" method="POST" onsubmit="y?">
                         <input type="hidden" name="id" value="${title}">
                         <input type="submit" value="delete">
-                    </form>`);
+                    </form>`, authStatusUI(isOwner));
                     response.writeHead(200);
                     response.end(html);
                 });
             });
         }
     } else if(pathname ==='/create'){
+        if(!authIsOwner(request, response)){
+            response.end('You need to login');
+            return false;
+        }
         fs.readdir('./data', function (err, filelist) {
             var title = 'WEB - create';
             var list = template.list(filelist);
@@ -58,7 +89,7 @@ var app = http.createServer(function (request, response) {
     <p>
         <input type="submit">
     </p>
-</form>`, ``);
+</form>`, ``, authStatusUI(isOwner));
             response.writeHead(200);
             response.end(html);
         });
@@ -82,6 +113,10 @@ var app = http.createServer(function (request, response) {
         
     }
     else if (pathname === `/update`) {
+        if(!authIsOwner(request, response)){
+            response.end('You need to login');
+            return false;
+        }
         fs.readdir('./data', function (err, filelist) {
             var filteredId = path.parse(queryData.id).base; // 경로 탐색 차단
             fs.readFile(`./data/${filteredId}`, 'utf8', function (err, description) {
@@ -98,7 +133,7 @@ var app = http.createServer(function (request, response) {
     <p>
         <input type="submit">
     </p>
-</form>`, ``);
+</form>`, ``, authStatusUI(isOwner));
             response.writeHead(200);
             response.end(html);
             });
@@ -126,6 +161,10 @@ var app = http.createServer(function (request, response) {
         });
     }
     else if(pathname=="/delete_process"){
+        if(!authIsOwner(request, response)){
+            response.end('You need to login');
+            return false;
+        }
         var body = '';
         request.on('data', function(data){
             body = body + data;
@@ -140,6 +179,61 @@ var app = http.createServer(function (request, response) {
             })
         });
         
+    }
+    else if(pathname=="/login"){
+        fs.readdir('./data', function(error, filelist){
+            var title = 'Login';
+            var list = template.list(filelist);
+            var html = template.html(title, list, `
+                <form action="login_process" method="POST">
+                    <p>
+                        <input type="text" name="email" placeholder="email">
+                    </p>
+                    <p>
+                        <input type="password" name="password" placeholder="passowrd">
+                    </p>
+                    <p>
+                        <input type="submit" vale="로그인">
+                    </p>
+                </form>
+            `, `<h2>Login</h2>`, authStatusUI(isOwner));
+            response.writeHead(200);
+            response.end(html);
+        });
+    }
+    else if(pathname=="/login_process"){
+        var body='';
+        request.on('data', function(data){
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            if(post.email == 'rudwns273@naver.com' && post.password=='111'){
+                console.log('loging...');
+                response.writeHead(302, {
+                    'Set-Cookie':[
+                        `email=${post.email}`,
+                        `password=${post.password}`,
+                        `nickname=rudwns`
+                    ],
+                    Location:'/'
+                });
+                response.end();
+            }else{
+                response.end('Login failed');
+            }
+        });
+    }
+    else if(pathname=='/logout_process'){
+        response.writeHead(302, {
+            'Set-Cookie':[
+                `email=; Max-Age=0`,
+                `password=; Max-Age=0`,
+                `nickname=; Max-Age=0`
+            ],
+            Location : '/'
+        });
+        response.end();
     }
     else {
         response.writeHead(404);
